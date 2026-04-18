16518b452baae1146773c0464240205a92a830f6
Alexander Sustavov <markowillz@gmail.com>
2026-04-18 08:58:28 +0600

    Fix Windows keys

--- internal/selector/keypress_windows.go
+++ internal/selector/keypress_windows.go
@@ -11,17 +11,44 @@ import (
 const (
 	enableEchoInput = 0x0004
 	enableLineInput = 0x0002
+
+	keyEvent = 0x0001
+	keyDown  = 1
+
+	vkLeft   = 0x25
+	vkUp     = 0x26
+	vkRight  = 0x27
+	vkDown   = 0x28
+	vkEscape = 0x1B
+	vkReturn = 0x0D
+	vkSpace  = 0x20
 )
 
 var (
-	kernel32           = syscall.NewLazyDLL("kernel32.dll")
-	procGetConsoleMode = kernel32.NewProc("GetConsoleMode")
-	procSetConsoleMode = kernel32.NewProc("SetConsoleMode")
+	kernel32             = syscall.NewLazyDLL("kernel32.dll")
+	procGetConsoleMode   = kernel32.NewProc("GetConsoleMode")
+	procSetConsoleMode   = kernel32.NewProc("SetConsoleMode")
+	procReadConsoleInput = kernel32.NewProc("ReadConsoleInputW")
 )
 
 type terminalState struct {
 	handle syscall.Handle
 	old    uint32
+}
+
+type inputRecord struct {
+	eventType uint16
+	_         uint16
+	key       keyEventRecord
+}
+
+type keyEventRecord struct {
+	keyDown         int32
+	repeatCount     uint16
+	virtualKeyCode  uint16
+	virtualScanCode uint16
+	unicodeChar     uint16
+	controlKeyState uint32
 }
 
 func enableRawMode() (*terminalState, error) {
@@ -61,30 +88,49 @@ func setConsoleMode(handle syscall.Handle, mode uint32) error {
 }
 
 func readKey() (rune, error) {
-	var b [1]byte
-	_, err := os.Stdin.Read(b[:])
-	if err != nil {
-		return 0, err
-	}
-	if b[0] != 0 && b[0] != 224 {
-		return readRuneFromFirstByte(b[0])
-	}
+	for {
+		var record inputRecord
+		var read uint32
 
-	_, err = os.Stdin.Read(b[:])
-	if err != nil {
-		return 0, err
-	}
+		if err := readConsoleInput(syscall.Handle(os.Stdin.Fd()), &record, &read); err != nil {
+			return 0, err
+		}
+		if read == 0 || record.eventType != keyEvent || record.key.keyDown != keyDown {
+			continue
+		}
 
-	switch b[0] {
-	case 72:
-		return keyArrowUp, nil
-	case 80:
-		return keyArrowDown, nil
-	case 77:
-		return keyArrowRight, nil
-	case 75:
-		return keyArrowLeft, nil
-	default:
-		return 0, nil
+		switch record.key.virtualKeyCode {
+		case vkUp:
+			return keyArrowUp, nil
+		case vkDown:
+			return keyArrowDown, nil
+		case vkRight:
+			return keyArrowRight, nil
+		case vkLeft:
+			return keyArrowLeft, nil
+		case vkEscape:
+			return keyEsc, nil
+		case vkReturn:
+			return '\r', nil
+		case vkSpace:
+			return ' ', nil
+		}
+
+		if record.key.unicodeChar != 0 {
+			return rune(record.key.unicodeChar), nil
+		}
 	}
+}
+
+func readConsoleInput(handle syscall.Handle, record *inputRecord, read *uint32) error {
+	r1, _, err := procReadConsoleInput.Call(
+		uintptr(handle),
+		uintptr(unsafe.Pointer(record)),
+		uintptr(1),
+		uintptr(unsafe.Pointer(read)),
+	)
+	if r1 == 0 {
+		return err
+	}
+	return nil
 }
